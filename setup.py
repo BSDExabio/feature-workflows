75679ce76e76cc2dcfbfd85d3eb24cc60a2f621e
Mark Coletti <colettima@ornl.gov>
2023-04-19 14:39:45 -0400

    Enhancements to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Load the version number from inside the package
 exec(open('workflows/__version__.py').read())
@@ -10,11 +10,27 @@ with open("README.md", "r") as f:
 setup(
     name='feature-workflows',
     version='',
-    packages=[''],
-    url='',
+    packages=find_packages(),
+    url='https://github.com/BSDExabio/feature-workflows',
     license='',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Russell B. Davidson, Mark Coletti, Ada Sedova',
     author_email='davidsonrb@ornl.gov, colettima@ornl.gov, sedovaaa@ornl.gov',
-    description='Protein feature extraction workflows'
+    description='Protein feature extraction workflows',
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+    install_requires=[
+        'dask[complete]',  # Used for parallel and distributed algorithms
+        'distributed',  # Used for parallel and distributed algorithms
+        'matplotlib',  # Used in visualizations
+        'numpy',  # Used for vector math
+        'pandas',  # Used to process CSV output
+        'rich',  # Used for pretty printing logs etc.
+        'scipy',
+        'toolz'  # All round useful extension to itertools and functools
+    ]
 )
